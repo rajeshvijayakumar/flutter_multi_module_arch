1b67cb15589bb2646b0701b4b060d2601c78125f
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-07-24 18:24:11 +0530

    [Dependency Management]:Configure-Dependency-Injection-for-Many-Environments-(Dev,Prod, test)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'di/injection.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies(Environment.dev);
   runApp(const MyApp());
 }
 
