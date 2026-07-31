6a81229559f304bc3d27634ddd4994091ee6cccc
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-07-31 13:08:29 +0530

    [Data Module]: Dio factory created & implemented inside the Data module.

---
+++ core/data/lib/constants/constants.dart
@@ -0,0 +1,11 @@
+
+const String contentType = "content-type";
+const String accept = "accept";
+const String authorization = "authorization";
+
+// value of the header
+const String applicationJson = "application/json";
+
+// custom headers
+const String defaultLanguage = "language";
+const String clientId = "clientId";
---
+++ core/data/lib/factory/dio_factory.dart
@@ -0,0 +1,40 @@
+
+import 'package:data/constants/constants.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  DioFactory();
+
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+
+    Map<String, String> headers = {
+      contentType: applicationJson,
+      accept: applicationJson,
+      authorization: "your_access_token", // todo get access token here.
+      defaultLanguage: "app_language", // todo get app lang here.
+      clientId: "app_client_id" // todo get app client id
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: "your_base_url", //todo add base url here.
+      headers: headers,
+      receiveTimeout: const Duration(seconds: 60),
+      sendTimeout: const Duration(seconds: 60),
+    );
+
+    // add logger  in debug mode only NOT release
+
+    if(!kReleaseMode) {
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true
+      ));
+    }
+
+    return dio;
+  }
+}
