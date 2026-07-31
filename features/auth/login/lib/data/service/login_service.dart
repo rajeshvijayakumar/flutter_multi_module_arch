22e4ad1a6142b0fcdf2d22a194720573ceb42bcc
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-07-31 14:03:18 +0530

    [Login Module]: Login Service created and added using retrofit library.

---
+++ features/auth/login/lib/data/service/login_service.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:retrofit/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import '../response/login_response.dart';
+
+part 'login_service.g.dart';
+
+const String baseUrl = "https://baseapiurl.io";
+
+@RestApi(baseUrl: baseUrl)
+abstract class LoginService {
+  factory LoginService(Dio dio,{String baseUrl}) = _LoginService;
+
+  @POST("/customers/login")
+  Future<HttpResponse<LoginResponse>> login(
+      @Field("email") String email, @Field("password") String password);
+}
