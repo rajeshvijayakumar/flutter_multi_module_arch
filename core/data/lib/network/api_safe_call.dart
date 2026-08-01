51e998c5f52521a8a9fa383ac22f57530d5f8f5b
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-01 11:37:32 +0530

    [Data Module]: Created safe api call with dartz either class for error handling..

---
+++ core/data/lib/network/api_safe_call.dart
@@ -0,0 +1,15 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:domain/model/failure.dart';
+import 'package:domain/model/localized_message.dart';
+
+Future<Either<Failure, T>> safeApiCall<T>(Future<T> Function() apiCall) async {
+  try {
+    final response = await apiCall();
+    return Right(response);
+  } catch (error) {
+    return Left(
+      Failure(0, LocalizedMessage("",""))  //todo error handler here
+    );
+  }
+}
