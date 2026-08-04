61c95b52925e25022de7cc07a43234fff29e52c9
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-04 12:57:56 +0530

    [Data Module]: Created enums and classes then response_code and messages then error messages under data module.

---
+++ core/data/lib/error_handler/data_source.dart
@@ -0,0 +1,16 @@
+
+enum DataSource {
+  success,
+  noContent,
+  badRequest,
+  forbidden,
+  unauthorised,
+  internalServerError,
+  connectTimeOut,
+  cancelled,
+  receiveTimeOut,
+  sendTimeOut,
+  cacheError,
+  noInternetConnection,
+  defaultError
+}
---
+++ core/data/lib/error_handler/response_messages.dart
@@ -0,0 +1,21 @@
+
+
+import 'error_messages.dart';
+
+class ResponseMessage {
+static const String success = AppStrings.success;
+static const String noContent = AppStrings.noContent;
+static const String badRequest = AppStrings.badRequest;
+static const String unauthorised = AppStrings.unauthorised;
+static const String internalServerError = AppStrings.internalServerError;
+static const String forbidden = AppStrings.forbidden;
+
+// local error form app
+static const String connectTimeOut = AppStrings.connectTimeOut;
+static const String cancelled = AppStrings.cancelled;
+static const String receiveTimeOut = AppStrings.receiveTimeOut;
+static const String sendTimeOut = AppStrings.sendTimeOut;
+static const String cacheError = AppStrings.cacheError;
+static const String noInternetConnection = AppStrings.noInternetConnection;
+static const String defaultError = AppStrings.defaultError;
+}
