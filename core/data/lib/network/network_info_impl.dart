0ff9173230c1619af18c12511ba9a65babbad5ed
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-04 12:20:55 +0530

    [Data Module]: Connectvity plus library added and network connectivity check wrapper implemented.

---
+++ core/data/lib/network/network_info_impl.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:data/network/network_info.dart';
+
+class NetworkInfoImpl extends NetworkInfo{
+
+  @override
+  Future<bool> get isConnected async {
+
+    final connectivityResult = await (Connectivity().checkConnectivity());
+
+    final isConnectedToNetwork = (
+        connectivityResult == ConnectivityResult.wifi
+            || connectivityResult == ConnectivityResult.mobile);
+
+    return isConnectedToNetwork;
+  }
+}
