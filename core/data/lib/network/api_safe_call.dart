68f3700fc09ca99f0f5cef118bbae965232f96ab
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-04 13:53:41 +0530

    [Data Module]: Integrated Dio and Unexpected Errors with Custom ErrorHandler for safe api call..

--- core/data/lib/network/api_safe_call.dart
+++ core/data/lib/network/api_safe_call.dart
@@ -1,5 +1,8 @@
 
 import 'package:dartz/dartz.dart';
+import 'package:data/error_handler/data_source.dart';
+import 'package:data/error_handler/data_source_extension.dart';
+import 'package:data/error_handler/dio_error_handler.dart';
 import 'package:data/network/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:domain/model/localized_message.dart';
@@ -11,15 +14,9 @@ Future<Either<Failure, T>> safeApiCall<T>(NetworkInfo networkInfo, Future<T> Fun
       final response = await apiCall();
       return Right(response);
     } catch (error) {
-      return Left(
-          Failure(0,  "") //todo error handler here
-      );
+      return Left(ErrorHandler.handle(error).failure);
     }
   } else {
-    return Left(
-        Failure(
-            -500,
-            "network connectivity issue, please check") //todo error handler here
-    );
+    return Left(DataSource.noInternetConnection.getFailure());
   }
 }
