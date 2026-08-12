693b230867bd98124d7eb5a2eebbbd0f281fdb51
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-12 14:32:50 +0530

    [Login Module]: Added login input validation inside bloc state class.

--- features/auth/login/lib/presentation/login_bloc.dart
+++ features/auth/login/lib/presentation/login_bloc.dart
@@ -12,17 +12,45 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
     //handle username changed event
     on<UserNameChanged>((event, emit){
-        //TODO logic here
+      final usernameError = validateUserName(event.userName);
+
+      emit(LoginInValid(userNameError: usernameError,
+      passwordError: (state is LoginInValid) ? state.passwordError : null));
+
     });
 
     //handle password changed event
     on<PasswordChanged>((event, emit){
-      //TODO logic here
+      final passwordError = validateUserName(event.password);
+
+      emit(LoginInValid(passwordError: passwordError,
+          userNameError: (state is LoginInValid) ? state.userNameError : null));
+
     });
 
     //handle login button press event
     on<LoginButtonPressed>((event, emit){
       //TODO logic here
     });
+  }
+
+  String? validateUserName(String userName) {
+    if (userName.isEmpty) {
+      return "Username cannot be empty";
+    } else if (userName.length < 3) {
+      return "Username must be at least 3 chars";
+    }
+
+    return null;
+  }
+
+  String? validatePassword(String password) {
+    if (password.isEmpty) {
+      return "password cannot be empty";
+    } else if (password.length < 6) {
+      return "password must be at least 6 chars";
+    }
+
+    return null;
   }
 }
