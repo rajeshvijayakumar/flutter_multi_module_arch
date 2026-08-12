46f2226b22a68d6f0d140527e833fa56be3a8f93
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-12 12:15:58 +0530

    [Data Module]:data module di import issue resolved.

--- core/data/lib/di/injection.dart
+++ core/data/lib/di/injection.dart
@@ -1,5 +1,5 @@
 import 'package:get_it/get_it.dart';
-import 'package:datastore/di/injection.config.dart';
+import 'package:data/di/injection.config.dart';
 import 'package:injectable/injectable.dart';
 
 @InjectableInit()
