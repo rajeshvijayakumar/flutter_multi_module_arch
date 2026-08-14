fab751765b39d5f3ee568dc3d21661faf93e8858
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-14 13:19:49 +0530

    [Presentation Module]: Handled State renderer missing usecases...

--- core/presentation/lib/state_renderer/state_renderer.dart
+++ core/presentation/lib/state_renderer/state_renderer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:presentation/state_renderer/state_renderer_type.dart';
 
@@ -9,40 +10,55 @@ class StateRenderer extends StatelessWidget {
 
   var _isDialogDismissed = false;
   var _isDialogShowing = false;
-
-  final VoidCallback retryActionFunction;
+  final VoidCallback? retryActionFunction;
 
   StateRenderer({
     super.key,
     required this.stateRendererType,
     this.message = "Loading...",
     this.title = "Error",
-    required this.retryActionFunction,
+    this.retryActionFunction,
   });
 
   @override
   Widget build(BuildContext context) {
+    if (_isDialogShowing) {
+      // because its rebuild again the screen when showing the dilaog so i put this condition to not show duplicated dialogs
+      _isDialogShowing = false;
+      return Container();
+    }
+
+    if (_isDialogDismissed) {
+      // because its rebuild again the screen when dismissing the dilaog so i put this condition to not show duplicated dialogs
+      _isDialogDismissed = false;
+      return Container();
+    }
+
+    // as a precaution only to dismiss any dilaog, to dimiss the dialog in case of showing the content of the screen
+    if (_isThereCurrentDialogShowing(context)) {
+      Navigator.of(context, rootNavigator: true).pop(true);
+    }
+
     switch (stateRendererType) {
       case StateRendererType.popupLoadingState:
         return _showPopupLoadingDialog(context, _buildLoadingWidget());
       case StateRendererType.popupErrorState:
-        return _showPopupErrorDialog(context, _buildErrorWidget());
+        if (_isThereCurrentDialogShowing(context)) {
+          Navigator.of(context, rootNavigator: true).pop(true);
+          return Container();
+        } else {
+          return _showPopupErrorDialog(context, _buildErrorWidget());
+        }
       case StateRendererType.fullScreenLoadingState:
         return _showFullScreenContent(_buildLoadingWidget());
-      case StateRendererType.fullScreenErrorState:
-        return _showFullScreenContent(_buildErrorWidget());
       case StateRendererType.fullScreenErrorState:
         return _showFullScreenContent(_buildErrorWidget(showRetryButton: true));
       case StateRendererType.emptyState:
         return _showFullScreenContent(_buildEmptyWidget());
       case StateRendererType.contentState:
-        // TODO: Handle this case.
-        throw UnimplementedError();
-      case StateRendererType.none:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+      default:
+        return Container(); // return empty container by default
     }
-    // return const Placeholder();
   }
 
   // it will check if we are displaying a dialog.
@@ -60,7 +76,6 @@ class StateRenderer extends StatelessWidget {
     );
   }
 
-  // it can show popup loading or popup error depending on "content"
   Widget _showPopupLoadingDialog(BuildContext context, Widget content) {
     if (!_isThereCurrentDialogShowing(context)) {
       _isDialogShowing = true;
@@ -72,7 +87,18 @@ class StateRenderer extends StatelessWidget {
         ),
       );
     }
-    return Container();
+    return Container(); // keep screen content behind the dialog
+  }
+
+  Widget _buildEmptyWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.inbox, size: 50, color: Colors.grey),
+        SizedBox(height: 10),
+        Text("No Data Available"),
+      ],
+    );
   }
 
   Widget _buildErrorWidget({bool showRetryButton = false}) {
@@ -87,9 +113,17 @@ class StateRenderer extends StatelessWidget {
         ),
         SizedBox(height: 10),
         Text(message),
+        SizedBox(height: 10),
         if (showRetryButton)
           ElevatedButton(onPressed: retryActionFunction, child: Text("Retry")),
       ],
+    );
+  }
+
+  Widget _showFullScreenContent(Widget content) {
+    return Container(
+      color: Colors.white,
+      child: Center(child: content),
     );
   }
 
@@ -119,23 +153,5 @@ class StateRenderer extends StatelessWidget {
       );
     }
     return Container(); // keep screen content behind the dialog
-  }
-
-  Widget _showFullScreenContent(Widget buildLoadingWidget) {
-    return Container(
-      color: Colors.white,
-      child: Center(child: buildLoadingWidget),
-    );
-  }
-
-  Widget _buildEmptyWidget() {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.inbox, size: 50, color: Colors.grey),
-        SizedBox(height: 10),
-        Text("No Data Available"),
-      ],
-    );
   }
 }
