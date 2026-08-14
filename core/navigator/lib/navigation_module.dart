112c58488c6c640f0f2377b2ad1a0b76dd7befcd
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-14 16:32:39 +0530

    [Navigation Module]: Implemented Navigation module and its structure....

---
+++ core/navigator/lib/navigation_module.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login/domain/usecase/login_usecase.dart';
+import 'package:login/presentation/login_screen.dart';
+import 'package:navigator/navigation_bloc.dart';
+import 'package:login/presentation/login_bloc.dart';
+import 'package:flutter_multi_module_arch/di/injection.dart';
+import 'package:navigator/navigation_routes.dart';
+import 'package:main/home_screen.dart';
+import 'package:navigator/navigation_state.dart';
+import 'package:navigator/navigation_types.dart';
+
+class NavigationModule extends StatelessWidget{
+
+  const NavigationModule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MultiBlocProvider(
+        providers: [
+          // provides navigation bloc
+          BlocProvider(create: (_) => NavigationBloc()),
+          // provides login bloc
+          BlocProvider(create: (_) => LoginBloc(getIt<LoginUseCase>())),
+          // provides home bloc
+          // BlocProvider(create: (_) => HomeBloc()), // provide home bloc
+        ],
+
+        child: MaterialApp(
+          routes: {
+            NavigationRoutes.home: (context) => HomeScreen(),
+            NavigationRoutes.login: (context) => LoginScreen(),
+
+          },
+          home: BlocListener<NavigationBloc, NavigationState>(
+            listener: (context, state){
+              if(state.route.isNotEmpty){
+                switch(state.navigationType){
+                case NavigationType.replace:
+                  Navigator.of(context).pushReplacementNamed(state.route);
+                  break;
+                case NavigationType.push:
+                  Navigator.of(context).pushNamed(state.route);
+                  break;
+                case NavigationType.pop:
+                  Navigator.of(context, rootNavigator: true).pop(true);
+                  break;
+                }
+              }
+            },
+            child: LoginScreen() // initial default screen
+          ),
+        )
+    );
+  }
+
+}
