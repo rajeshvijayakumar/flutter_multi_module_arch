95c48f3ad9066d0dc707236845ac5ec80d3bc58f
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-14 12:27:18 +0530

    [Presentation Module]: Loading popup loading state ...

--- core/presentation/lib/state_renderer/state_renderer.dart
+++ core/presentation/lib/state_renderer/state_renderer.dart
@@ -2,40 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:presentation/state_renderer/state_renderer_type.dart';
 
 class StateRenderer extends StatelessWidget {
-
   final StateRendererType stateRendererType;
+
+  var _isDialogMissed = false;
+  var _isDialogShowing = false;
 
   StateRenderer({super.key, required this.stateRendererType});
 
   @override
   Widget build(BuildContext context) {
-
-    switch(stateRendererType) {
+    switch (stateRendererType) {
       case StateRendererType.popupLoadingState:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _showPopupDialog(context, _buildLoadingWidget());
       case StateRendererType.popupErrorState:
         // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.fullScreenLoadingState:
-      // TODO: Handle this case.
+        // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.fullScreenErrorState:
-      // TODO: Handle this case.
+        // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.fullScreenErrorState:
-      // TODO: Handle this case.
+        // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.emptyState:
-      // TODO: Handle this case.
+        // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.contentState:
-      // TODO: Handle this case.
+        // TODO: Handle this case.
         throw UnimplementedError();
       case StateRendererType.none:
-      // TODO: Handle this case.
+        // TODO: Handle this case.
         throw UnimplementedError();
     }
     // return const Placeholder();
+  }
+
+  // it will check if we are displaying a dialog.
+  bool _isThereCurrentDialogShowing(BuildContext context) =>
+      ModalRoute.of(context)?.isCurrent != true;
+
+  Widget _buildLoadingWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CircularProgressIndicator(),
+        SizedBox(height: 10),
+        Text("Loading..."),
+      ],
+    );
+  }
+
+  // it can show popup loading or popup error depending on "content"
+  Widget _showPopupDialog(BuildContext context, Widget content) {
+    if (!_isThereCurrentDialogShowing(context)) {
+      _isDialogShowing = true;
+
+      WidgetsBinding.instance.addPersistentFrameCallback(
+        (_) => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(content: content),
+        ),
+      );
+    }
+    return Container();
   }
 }
