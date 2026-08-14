6bfa4a368079ba35111721a7b6ab6f83b2a9e8b2
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-14 14:45:36 +0530

    [Navigation Module]: created navigation events, states and types - enum.

---
+++ core/navigator/lib/navigation_event.dart
@@ -0,0 +1,16 @@
+
+import 'package:navigator/navigation_types.dart';
+
+abstract class NavigationEvent {}
+
+class NavigateToHome implements NavigationEvent {}
+
+class NavigateToLogin implements NavigationEvent {}
+
+class NavigateToRoute implements NavigationEvent {
+  final String route;
+  final NavigationType navigationType;
+
+  NavigateToRoute(this.route, this.navigationType);
+}
+
---
+++ core/navigator/lib/navigation_types.dart
@@ -0,0 +1,6 @@
+
+enum NavigationType {
+  push,
+  replace,
+  pop
+}
