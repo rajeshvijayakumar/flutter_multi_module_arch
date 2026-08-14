4a1f598c7478c0ace2bcd7af15e0bfd47b62ad4e
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-14 13:32:54 +0530

    [Login Module]: Updated the login state with the state renderer..

--- features/auth/login/lib/presentation/login_state.dart
+++ features/auth/login/lib/presentation/login_state.dart
@@ -1,36 +1,53 @@
 import 'package:equatable/equatable.dart';
+import 'package:presentation/state_renderer/state_renderer_type.dart';
 
 class LoginState extends Equatable {
   final String? userNameError;
   final String? passwordError;
   final String? errorMessage;
+  final StateRendererType? stateRendererType;
 
-  const LoginState({this.userNameError, this.passwordError, this.errorMessage});
+  const LoginState({
+    this.userNameError,
+    this.passwordError,
+    this.errorMessage,
+    this.stateRendererType = StateRendererType.contentState});
 
   LoginState copyWith({
     String? userNameError,
     String? passwordError,
     String? errorMessage,
+    StateRendererType? stateRendererType
   }) {
     return LoginState(
       userNameError: userNameError,
       passwordError: passwordError,
       errorMessage: errorMessage,
+      stateRendererType: stateRendererType
     );
   }
 
   @override
-  List<Object?> get props => [userNameError, passwordError, errorMessage];
+  List<Object?> get props => [userNameError, passwordError, errorMessage, stateRendererType];
 }
 
 //initial state
-class LoginInitial extends LoginState {}
+class LoginInitial extends LoginState {
+  const LoginInitial()
+      : super(stateRendererType: StateRendererType.contentState);
+}
 
 //loading state
-class LoginLoading extends LoginState {}
+class LoginLoading extends LoginState {
+  const LoginLoading()
+      : super(stateRendererType: StateRendererType.popupLoadingState);
+}
 
 //success state
-class LoginSuccess extends LoginState {}
+class LoginSuccess extends LoginState {
+  const LoginSuccess()
+      : super(stateRendererType: StateRendererType.contentState);
+}
 
 
 //invalid input state
@@ -40,12 +57,17 @@ class LoginInValid extends LoginState {
   final String? passwordError;
 
   const LoginInValid({this.userNameError, this.passwordError})
-      : super(userNameError: userNameError, passwordError: passwordError);
+      : super(userNameError: userNameError,
+              passwordError: passwordError,
+              stateRendererType: StateRendererType.contentState);
 }
 
 //error state
 class LoginError extends LoginState {
   final String? errorMessage;
 
-  const LoginError({this.errorMessage}) : super(errorMessage: errorMessage);
+  const LoginError({this.errorMessage})
+      : super(
+            errorMessage: errorMessage,
+            stateRendererType: StateRendererType.popupErrorState);
 }
