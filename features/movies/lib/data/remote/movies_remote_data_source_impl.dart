5303b6a02f7c1d3c0934f3d315e96172f830814c
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-18 17:54:12 +0530

    [Movies Module]: Created/Added Movie remote data source abstraction layer...

---
+++ features/movies/lib/data/remote/movies_remote_data_source_impl.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:domain/model/failure.dart';
+import 'package:data/network/api_safe_call.dart';
+import 'package:data/network/network_info.dart';
+import 'package:movies/data/response/movies_response.dart';
+import 'package:movies/data/service/movies_service.dart';
+
+import 'movies_remote_data_source.dart';
+
+class MoviesRemoteDataSourceImpl implements MoviesRemoteDataSource {
+  final MoviesService moviesService;
+  final NetworkInfo networkInfo;
+
+  MoviesRemoteDataSourceImpl(this.moviesService, this.networkInfo);
+
+  @override
+  Future<Either<Failure, MoviesResponse>> getMovies() {
+    return safeApiCall(networkInfo, () async {
+      final response = await moviesService.getMovies();
+      return response.data;
+    });
+  }
+}
