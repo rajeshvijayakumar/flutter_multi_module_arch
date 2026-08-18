049aeb58462a4a6e64cf26c1a18266ea7cc9d90a
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-18 16:45:03 +0530

    [Movies Module]: Created/Added Movie domain presentation model class..

---
+++ features/movies/lib/domain/movie.dart
@@ -0,0 +1,19 @@
+class Movie {
+  String id;
+  String originalTitle;
+  String overview;
+  String posterPath;
+  String releaseDate;
+  String title;
+  String voteAverage;
+
+  Movie({
+    required this.id,
+    required this.originalTitle,
+    required this.overview,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.voteAverage,
+  });
+}
