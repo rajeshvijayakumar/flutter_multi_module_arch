ab77ec694a9cdbddb181126aed8f47baa64bbf67
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-19 15:59:29 +0530

    [Movies Module]: Testing Movies List UI and refactored the build issues.

--- features/movies/lib/presentation/ui/movies_screen.dart
+++ features/movies/lib/presentation/ui/movies_screen.dart
@@ -28,17 +28,14 @@ class MoviesScreen extends StatelessWidget {
     });
 
     return Scaffold(
-      body: BlocProvider(
-        create: (context) => MoviesBloc(moviesUseCase),
-        child: BlocBuilder<MoviesBloc, MoviesState>(builder: (context, state) {
-          return Stack(
-            children: [
-              if (state is MoviesSuccess) _buildMainScreenContent(context, state),
-              _buildStateRenderer(context, state)
-            ],
-          );
-        }),
-      ),
+      body: BlocBuilder<MoviesBloc, MoviesState>(builder: (context, state) {
+        return Stack(
+          children: [
+            if (state is MoviesSuccess) _buildMainScreenContent(context, state),
+            _buildStateRenderer(context, state)
+          ],
+        );
+      }),
     );
   }
 
