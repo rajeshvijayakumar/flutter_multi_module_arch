8f899a547ae3e6d1f377aa0fa4cd29ea311cc11a
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-08-20 10:17:24 +0530

    [Movies Module]: Created movie local data source implementation with expiry.

---
+++ features/movies/lib/data/source/cache/movies_cache_data_source_impl.dart
@@ -0,0 +1,50 @@
+
+
+import 'package:hive_ce/hive.dart';
+import 'package:movies/data/source/cache/movies_cache_data_source.dart';
+import 'package:movies/domain/model/cached_movie.dart';
+
+class MoviesCacheDataSourceImpl implements MoviesCacheDataSource {
+
+  static const _moviesCacheKey = 'moviesCacheKey';
+  static const _cacheExpiryKey = 'cacheExpiryKey';
+  static const _moviesCacheExpiryInSeconds = 60;
+  final Box _box;
+
+  MoviesCacheDataSourceImpl(this._box);
+
+  @override
+  Future<void> cacheMovies(List<CachedMovie> movies) async {
+
+    // store movies
+    await _box.put(_moviesCacheKey, movies);
+
+    // set expiry time
+    await _box.put(
+        _cacheExpiryKey,
+        DateTime.now()
+            .add(const Duration(seconds: _moviesCacheExpiryInSeconds))
+            .toIso8601String()); // 3:00 + 10 == 3:10 pm
+  }
+
+  @override
+  Future<List<CachedMovie>> getMovies() async {
+
+    return (_box.get(_moviesCacheKey, defaultValue: <CachedMovie>[]) as List)
+        .cast<CachedMovie>();
+  }
+
+  @override
+  Future<bool> hasValidCache() async {
+
+    //read the expiry time
+    final cacheExpiryTimeString = _box.get(_cacheExpiryKey);
+    if (cacheExpiryTimeString == null) return false;
+
+    //parse
+    final cacheExpiryTime = DateTime.parse(cacheExpiryTimeString);
+    // DateTime.now() => 3:11
+    // cacheExpiryTime => 3:10
+    return DateTime.now().isBefore(cacheExpiryTime); // returns true if it is before time //check the expiry time
+  }
+}
